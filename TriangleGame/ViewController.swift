974ce7f317c8d00426ca5b6b60271543eb5ed727
StasyHard <reyngardt.as@yandex.ru>
2020-04-07 16:59:34 +0300

    Initial

---
+++ TriangleGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriangleGame
+//
+//  Created by Anastasia Reyngardt on 07.04.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
